b693d5ede96a52c3ffc8585658d1778c0d4143c5
Ilya <varsto@localhost.localdomain>
2022-04-02 00:54:26 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 1920
+        self.screen_height = 1000
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 5.5
+        self.ship_limit = 3
+        self.bullet_speed = 8
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (0, 191, 255)
+        self.alien_speed = 3.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
